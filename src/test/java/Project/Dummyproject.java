bb72df936d44db71984b9dc6114b6a6c37c58ef0
Vinoth12345678900 <strvinoth358@gmail.com>
2022-09-08 11:27:50 +0530

    vinoth code

---
+++ src/test/java/Project/Dummyproject.java
@@ -0,0 +1,15 @@
+package Project;
+public  class Dummyproject {
+	int a=10;
+	int b=20;
+	int c=30;
+	public static void main(String[] args) {
+		System.out.println("test1");
+		System.out.println("test2");
+	}
+	
+	
+	
+	
+	
+	}
